199979ce3aa692dc67e958f16389ea46e268495a
nglequy <nglequy@email.com>
2022-04-14 22:43:55 +0700

    add main fragment

--- app/src/main/java/com/example/android5/SplashActivity.kt
+++ app/src/main/java/com/example/android5/SplashActivity.kt
@@ -3,14 +3,16 @@ package com.example.android5
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Handler().postDelayed(Runnable {
-            var intent = Intent(this, NewfeedFragment::class.java)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Your Code
+            var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }, 2000)
     }
